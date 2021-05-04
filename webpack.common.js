143ea33cf85980ca02f82c7da94244efeb173ad1
piyo <chick.p.1120@gmail.com>
2021-05-04 20:38:41 +0900

    chore: initialize react app (#1)
    * deps: react react-dom
    
    * deps: webpack webpack-cli
    
    * deps: typescript
    
    * deps: ts-loader
    
    * deps: @types/{react,react-dom}
    
    * deps: eslint prettier
    
    * deps: npm-run-all
    
    * deps: eslint-plugin-react
    
    * deps: eslint-plugin-import
    
    * chore: add .eslintrc.js
    
    * chore: add tsconfig.json
    
    * deps: clean-webpack-plugin
    
    * deps: webpack-merge
    
    * deps: webpack-dev-server
    
    * chore: webpack config
    
    * chore: add npm-script for build
    
    * chore: add `dist` into .gitignore
    
    * chore: add example
    
    * chore: add npm-script for lint

---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
---
+++ webpack.dev.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+  },
+});
