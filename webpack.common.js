565f300556b667f0f00bc7fa0226d5f164f23a2b
chick-p <chick.p.1120@gmail.com>
2021-05-06 19:46:24 +0900

    chore(fix): not use dotenv in production mode

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
-const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const dotenv = require("dotenv");
-
-const env = dotenv.config().parsed;
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -36,10 +32,5 @@ module.exports = {
     path: path.join(__dirname, "dist"),
     publicPath: "/dist/",
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new webpack.DefinePlugin({
-      "process.env": JSON.stringify(env),
-    }),
-  ],
+  plugins: [new CleanWebpackPlugin()],
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const { merge } = require("webpack-merge");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+
 const common = require("./webpack.common.js");
+
+const env = dotenv.config().parsed;
 
 module.exports = merge(common, {
   mode: "development",
@@ -9,4 +14,10 @@ module.exports = merge(common, {
   devServer: {
     contentBase: path.join(__dirname, "public"),
   },
+  plugins: [
+    ...common.plugins,
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(env),
+    }),
+  ],
 });
