9025e4aa1700c591c74f26e8ca347698620b2011
piyo <chick.p.1120@gmail.com>
2021-05-06 19:52:13 +0900

    chore(fix): not use dotenv in production mode (#10)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { merge } = require("webpack-merge");
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   plugins: [
     ...common.plugins,
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+    }),
     new CopyPlugin({
-      patterns: [
-        { from: "public", to: "./" },
-      ],
-    })
+      patterns: [{ from: "public", to: "./" }],
+    }),
   ],
 });
