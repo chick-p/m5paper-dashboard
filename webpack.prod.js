75c0ee08dc24b64acfa6d1c982649eba61e9e3b7
piyo <chick.p.1120@gmail.com>
2021-05-06 19:22:44 +0900

    chore: copy `pubic` to `dist` (#9)
    * deps: copy-webpack-plugin
    
    * chore: copy public to dist
    
    * fix: js path

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { merge } = require("webpack-merge");
+const CopyPlugin = require('copy-webpack-plugin');
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    ...common.plugins,
+    new CopyPlugin({
+      patterns: [
+        { from: "public", to: "./" },
+      ],
+    })
+  ],
 });
