fb5ba6a231590c1a446bb9e0017e082ef6e59f8c
chick-p <chick.p.1120@gmail.com>
2021-05-09 16:55:20 +0900

    feat: upload png for m5paper

--- screenshot/handler.ts
+++ screenshot/handler.ts
@@ -1,17 +1,40 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
+import chromium from "chrome-aws-lambda";
+import AWS from "aws-sdk";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message:
-          "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const { S3_BUCKET_NAME, URL } = process.env;
+
+export const screenshot = async (): Promise<void> => {
+  AWS.config.update({ region: process.env.AWS_REGION || "ap-northeast-1" });
+  try {
+    await chromium.font(
+      "https://raw.githack.com/googlefonts/noto-cjk/main/Sans/OTF/Japanese/NotoSansCJKjp-Regular.otf"
+    );
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.goto(URL, {
+      waitUntil: "networkidle0",
+    });
+    const data = (await page.screenshot({ fullPage: true })) as Buffer;
+    await browser.close();
+    const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+    const resp = await s3
+      .upload({
+        Bucket: S3_BUCKET_NAME,
+        Key: "m5paper.png",
+        Body: data,
+        ContentType: "image/png",
+        ACL: "public-read",
+      })
+      .promise();
+    console.log(resp);
+  } catch (err) {
+    console.error(err);
+  }
 };
