7ed4da7b0e42a579337828f8a70e0756d99045fd
chick-p <chick.p.1120@gmail.com>
2021-05-11 21:10:48 +0900

    feat: add TodoistPanel to App

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -8,8 +8,8 @@ export const App: React.FC = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-        {/* <DayPanel />
-        <WeatherPanel /> */}
+        <DayPanel />
+        <WeatherPanel />
         <TodoistPanel />
       </div>
     </div>
