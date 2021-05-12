fc39a542fd751c0086511ea8e84bfe50dd5bb829
chick-p <chick.p.1120@gmail.com>
2021-05-12 19:36:28 +0900

    fix: date format

--- dashboard/src/components/DayPanel/index.tsx
+++ dashboard/src/components/DayPanel/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { format } from "date-fns";
-import ja from "date-fns/locale/ja";
 
 const Component: React.FC = () => {
   const now = new Date();
-  const day = format(now, "yyyy年MM月dd日");
-  const week = format(now, "eeee", { locale: ja });
+  const day = format(now, "MM/dd");
+  const week = format(now, "eeee");
   return (
     <div className="col-start-1 col-span-1 relative bg-white py-6 px-6 rounded-3xl border-2 border-gray-500 my-4 align-text-top">
       <div>
