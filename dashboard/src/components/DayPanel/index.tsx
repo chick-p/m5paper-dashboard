18bee4585cb0df541c2589b098662e5bf5105c52
chick-p <chick.p.1120@gmail.com>
2021-05-19 19:25:52 +0900

    fix: consider timezone

--- dashboard/src/components/DayPanel/index.tsx
+++ dashboard/src/components/DayPanel/index.tsx
@@ -4,8 +4,8 @@ import ja from "date-fns/locale/ja";
 
 const Component: React.FC = () => {
   const now = new Date();
-  const day = format(now, "MM/dd");
-  const week = format(now, "eeee");
+  const day = format(now, "MM/dd", { locale: ja });
+  const week = format(now, "eeee", { locale: ja });
   const time = format(now, "HH:mm", { locale: ja });
   return (
     <div className="col-start-1 col-span-1 relative bg-white py-6 px-6 rounded-3xl border-2 border-gray-500 my-4 align-text-top">
