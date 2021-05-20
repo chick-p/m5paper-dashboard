acdf5f2e9e458f5246245a383dfcafbcb9eac9dd
piyo <chick.p.1120@gmail.com>
2021-05-20 20:32:31 +0900

    feat: show time in DayPanel (#19)
    * feat: add time in DayPanel
    
    * fix: consider timezone

--- dashboard/src/components/DayPanel/index.tsx
+++ dashboard/src/components/DayPanel/index.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { format } from "date-fns";
+import ja from "date-fns/locale/ja";
 
 const Component: React.FC = () => {
   const now = new Date();
-  const day = format(now, "MM/dd");
-  const week = format(now, "eeee");
+  const day = format(now, "MM/dd", { locale: ja });
+  const week = format(now, "eeee", { locale: ja });
+  const time = format(now, "HH:mm", { locale: ja });
   return (
     <div className="col-start-1 col-span-1 relative bg-white py-6 px-6 rounded-3xl border-2 border-gray-500 my-4 align-text-top">
-      <div>
-        <p className="text-gray-700 text-2xl font-semibold mb-2">{day}</p>
-        <div className="flex space-x-2 text-gray-700 text-xl">
-          <p>{week}</p>
+      <div className="text-gray-700">
+        <div className="flex items-center space-x-2 mb-2">
+          <p className="font-semibold text-2xl">{day}</p>
+          <p className="text-xl">{week}</p>
+        </div>
+        <div className="flex space-x-2 text-xl">
+          <p>{time}</p>
         </div>
       </div>
     </div>
